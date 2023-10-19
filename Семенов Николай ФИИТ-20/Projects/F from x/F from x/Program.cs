e5bb825da7d58a00d8286f6622cf7df85e65c109
flytkins <113571988+flytkins@users.noreply.github.com>
2023-10-19 16:12:13 +0900

    добавил колю

---
+++ Семенов Николай ФИИТ-20/Projects/F from x/F from x/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace F_from_x
+{
+    class Program
+    {
+        static void Func(double x, double a, double d)
+        {
+            if (a >= -3 && a <= 23)
+            {
+                Console.WriteLine("Parameter 'a' entered incorrectly");
+                return;
+            }
+            if (d == Math.Pow(25, 1.0 / 3))
+            {
+                Console.WriteLine("Parameter 'd' entered incorrectly");
+                return;
+            }
+            Console.WriteLine((5 / (d * d * d - 25)) * x * x * x + (7 / Math.Sqrt(a * a - 20 * a - 69)) * x * x - 3 * x - 20); 
+        }
+        static void Main(string[] args)
+        {
+            double x, a, d;
+            Console.WriteLine("Write x:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Write a:");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Write d:");
+            d = Convert.ToDouble(Console.ReadLine());
+            Func(x, a, d);
+        }
+    }
+}
---
+++ Семенов Николай ФИИТ-20/Projects/F from x/F from x/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("F from x")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("F from x")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("72b84ec7-08c8-41c1-82be-56549e23d473")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
