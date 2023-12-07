0b17cd9fcd2a11ba65cbd19ccc7515979f7e9e43
SemNik911 <sna911vk@gmail.com>
2023-12-07 16:19:14 +0900

    реализовал вторую функуцию в MAIN

--- Семенов Николай ФИИТ-20/Projects/F from x/F from x/Program.cs
+++ Семенов Николай ФИИТ-20/Projects/F from x/F from x/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 namespace F_from_x
 {
@@ -9,30 +10,41 @@ namespace F_from_x
     {
         public static double Func(double x, double a, double d)
         {
-            //if (a > -3 && a < 23)
-            //{
-            //    return double.NaN;
-            //}
-            //if (a == -3 && a == 23)
-            //{
-            //    return double.PositiveInfinity;
-            //}
-            //if (d == Math.Pow(25, 1.0 / 3))
-            //{
-            //    return double.NaN;
-            //}
             return (5 / (d * d * d - 25)) * x * x * x + (7 / Math.Sqrt(a * a - 20 * a - 69)) * x * x - 3 * x - 20;
+        }
+        public static double[] Razb(double x1, double x2, int n, double a, double d)
+        {
+            double[] diapazone = new double[n];
+            double h = (x2 - x1) / n - 1;
+            for (int i = 0; i < n; i++)
+            {
+                diapazone[i] = Func(x1 + h * i, a, d);
+            }
+            return diapazone;
         }
         static void Main(string[] args)
         {
-            double x, a, d;
-            Console.WriteLine("Write x:");
-            x = Convert.ToDouble(Console.ReadLine());
+            double a, d;
+            //Console.WriteLine("Write x:");
+            //x = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Write a:");
             a = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Write d:");
             d = Convert.ToDouble(Console.ReadLine());
-            Func(x, a, d);
+            //Func(x, a, d);
+            double x1, x2;
+            int n;
+            Console.WriteLine("Write x1:");
+            x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Write x2:");
+            x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Write n:");
+            n = Convert.ToInt32(Console.ReadLine());
+            double[] diapazone = Razb(x1, x2, n, a, d);
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(diapazone[i] + " ");
+            }
         }
     }
 }
