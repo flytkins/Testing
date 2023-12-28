eb00c4988e1f22577c701716a384f2063be3d9da
flytkins <flytkins@gmail.com>
2023-12-29 07:07:14 +0900

    array function

--- FuncRealization/FuncRealization/FunctionClass.cs
+++ FuncRealization/FuncRealization/FunctionClass.cs
@@ -12,5 +12,15 @@ namespace FuncRealization
         {
             return Math.Exp(Math.Sqrt(c * c - 3 * c + 2) / (b - 10) * x);
         }
+        public static double[] Razb(double x1, double x2, int n, double c, double b)
+        {
+            double[] diapazone = new double[n];
+            double h = (x2 - x1) / n - 1;
+            for (int i = 0; i < n; i++)
+            {
+                diapazone[i] = func(x1 + h * i, c, b);
+            }
+            return diapazone;
+        }
     }
 }
